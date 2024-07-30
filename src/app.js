730e8f0d39dc09c67d733f416e4ca3b5d054a4c7
DioChuks <diochuks65@gmail.com>
2024-07-30 14:10:30 +0100

    chore: swagger update

--- src/app.js
+++ src/app.js
@@ -92,7 +92,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true })
 
 app.get('/swagger.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerJsdoc);
+  res.send(specs);
 });
 
 // send back a 404 error for any unknown api request
