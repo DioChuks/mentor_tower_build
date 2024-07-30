60a5fef5fd6d1a471777ef9f9545422f5e81deb4
DioChuks <diochuks65@gmail.com>
2024-07-30 03:01:49 +0100

    fix: an issue with router having undeifined object

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -1,67 +1,76 @@
-const CourseService = require('../services/CourseService');
+const CourseService = require('../services/CourseService')
 
-exports.createJob = async (req, res, next) => {
+const createCourse = async (req, res, next) => {
   try {
-    const job = await CourseService.createCourse(req.body);
-    res.status(201).json(Course);
+    const job = await CourseService.createCourse(req.body)
+    res.status(201).json(Course)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-exports.getCourses = async (req, res, next) => {
+const getCourses = async (req, res, next) => {
   try {
-    const Courses = await CourseService.getCourses();
-    res.status(200).json(Courses);
+    const Courses = await CourseService.getCourses()
+    res.status(200).json(Courses)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-exports.getCourseById = async (req, res, next) => {
+const getCourseById = async (req, res, next) => {
   try {
-    const Course = await CourseService.getCourseById(req.params.id);
+    const Course = await CourseService.getCourseById(req.params.id)
     if (!Course) {
-      return res.status(404).json({ message: 'Course not found' });
+      return res.status(404).json({ message: 'Course not found' })
     }
-    res.status(200).json(Course);
+    res.status(200).json(Course)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-exports.getCoursesByPoster = async (req, res, next) => {
+const getCoursesByOwner = async (req, res, next) => {
   try {
-    const Course = await CourseService.getCoursesByOwner(req.params.id);
+    const Course = await CourseService.getCoursesByOwner(req.params.id)
     if (!Course) {
-      return res.status(404).json({ message: 'Course not found' });
+      return res.status(404).json({ message: 'Course not found' })
     }
-    res.status(200).json(Course);
+    res.status(200).json(Course)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-exports.updateCourse = async (req, res, next) => {
+const updateCourse = async (req, res, next) => {
   try {
-    const Course = await CourseService.updateCourse(req.params.id, req.body);
+    const Course = await CourseService.updateCourse(req.params.id, req.body)
     if (!Course) {
-      return res.status(404).json({ message: 'Course not found' });
+      return res.status(404).json({ message: 'Course not found' })
     }
-    res.status(200).json(Course);
+    res.status(200).json(Course)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-exports.deleteCourse = async (req, res, next) => {
+const deleteCourse = async (req, res, next) => {
   try {
-    const Course = await CourseService.deleteCourse(req.params.id);
+    const Course = await CourseService.deleteCourse(req.params.id)
     if (!Course) {
-      return res.status(404).json({ message: 'Course not found' });
+      return res.status(404).json({ message: 'Course not found' })
     }
-    res.status(200).json({ message: 'Course deleted successfully' });
+    res.status(200).json({ message: 'Course deleted successfully' })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
+
+module.exports = {
+  createCourse,
+  getCourses,
+  getCourseById,
+  getCoursesByOwner,
+  updateCourse,
+  deleteCourse
+}
--- src/models/User.js
+++ src/models/User.js
@@ -4,79 +4,81 @@ const { UserRole } = require('../contracts/roles')
 const toJSON = require('../config/toJson')
 const { compare, hash } = require('bcrypt')
 
-
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-            throw new Error('Invalid email');
-            }
-        },
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Invalid email')
+        }
+      }
     },
     isEmailVerified: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false
     },
     phone: {
-        type: Number,
-        required: false
+      type: Number,
+      required: false
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     role: {
-        type: String,
-        enum: Object.values(UserRole),
-        default: UserRole.Mentee
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.Mentee
     },
     dob: {
-        type: Date,
-        required: false,
+      type: Date,
+      required: false
     },
     bio: {
-        type: String,
-        required: false
+      type: String,
+      required: false
     },
     tier: {
-        type: String,
-        required: false
+      type: String,
+      required: false
     },
     areaOfInterest: {
-        type: String,
-        required: false
+      type: String,
+      required: false
     }
-}, { timestamps: true });
+  },
+  { timestamps: true }
+)
 
-UserSchema.plugin(toJSON);
+UserSchema.plugin(toJSON)
 
 UserSchema.static('isEmailTaken', async function (email, excludeUserId) {
-    const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
-    return !!user;
-  });
+  const user = await this.findOne({ email, _id: { $ne: excludeUserId } })
+  return !!user
+})
 
 UserSchema.method('isPasswordMatch', async function (password) {
-    const user = this;
-    return compare(password, user.password);
-});
+  const user = this
+  return compare(password, user.password)
+})
 
 UserSchema.pre('save', async function (next) {
-    const user = this;
-    if (user.isModified('password')) {
-      user.password = await hash(user.password, 8);
-    }
-    next();
-});
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await hash(user.password, 8)
+  }
+  next()
+})
 
-const User = model('User', UserSchema);
+const User = model('User', UserSchema)
 
-module.exports = User;
+module.exports = User
