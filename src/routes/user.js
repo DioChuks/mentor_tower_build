c018fb2477f6bd57a98da32ea7dd3cfd3000f9b1
DioChuks <diochuks65@gmail.com>
2024-08-05 07:09:13 +0100

    chore: swagger docs for this

--- src/routes/user.js
+++ src/routes/user.js
@@ -17,3 +17,74 @@ router.patch(
 router.get('/:userId', userController.getUserProfile)
 
 module.exports = router
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management and retrieval
+ */
+
+/**
+ * @swagger
+ * /users/:userId:
+ *   get:
+ *     summary: Get user profile
+ *     description: Retrieve the full user profile.
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The user profile.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: User not found.
+ */
+
+/**
+ * @swagger
+ * /users/:userId:
+ *   patch:
+ *     summary: Update user profile
+ *     description: Update the user profile data.
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               phone:
+ *                 type: number
+ *               password:
+ *                 type: string
+ *               dob:
+ *                 type: string
+ *                 format: date
+ *               bio:
+ *                 type: string
+ *               tier:
+ *                 type: string
+ *               areaOfInterest:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *     responses:
+ *       200:
+ *         description: The updated user profile.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Invalid updates.
+ *       404:
+ *         description: User not found.
+ */
