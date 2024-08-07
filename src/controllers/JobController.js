3d0df578fa16b84c33fc67fddfa5929925226df2
DioChuks <diochuks65@gmail.com>
2024-08-07 09:16:22 +0100

    chore: catch error with Error middleware

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -5,7 +5,7 @@ exports.createJob = async (req, res, next) => {
     const job = await JobService.createJob(req.body);
     res.status(201).json(job);
   } catch (error) {
-    next(error);
+    throw new ApiError(StatusCodes.BAD_REQUEST, error.message);
   }
 };
 
